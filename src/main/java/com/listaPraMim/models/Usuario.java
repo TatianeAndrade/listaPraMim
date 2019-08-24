cfb33e59357d9adaeb16df2db70d1c0159231907
marsjunior <marcos.junio@ccc.ufcg.edu.br>
2019-08-24 20:51:22 -0300

    Cria modelo do usuario

---
+++ src/main/java/com/listaPraMim/models/Usuario.java
@@ -0,0 +1,53 @@
+package com.listaPraMim.models;
+
+import java.io.Serializable;
+
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
+import javax.persistence.Id;
+import javax.validation.constraints.NotEmpty;
+import javax.validation.constraints.NotNull;
+
+@Entity
+public class Usuario implements Serializable{
+	
+	private static final long serialVersionUID = 1L;
+	
+	@Id
+	@GeneratedValue(strategy = GenerationType.AUTO)
+	private long id;
+	
+	@NotNull
+	@NotEmpty
+	private String nome;
+	
+	@NotNull
+	@NotEmpty
+	private String email;
+	
+	@NotNull
+	@NotEmpty
+	private String senha;
+	
+	public String getNome() {
+		return nome;
+	}
+	public void setNome(String nome) {
+		this.nome = nome;
+	}
+	public String getEmail() {
+		return email;
+	}
+	public void setEmail(String email) {
+		this.email = email;
+	}
+	public String getSenha() {
+		return senha;
+	}
+	public void setSenha(String senha) {
+		this.senha = senha;
+	}
+	
+	
+}
