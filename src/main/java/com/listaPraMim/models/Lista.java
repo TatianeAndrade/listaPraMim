b39f84c523f02b14be63dd99162889b08bb96fbe
TatianeAndrade <tatiane.ferreira@ccc.ufcg.edu.br>
2019-08-25 20:09:25 -0300

    Cria modelo de lista

---
+++ src/main/java/com/listaPraMim/models/Lista.java
@@ -0,0 +1,47 @@
+package com.listaPraMim.models;
+
+import java.io.Serializable;
+import java.util.List;
+
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
+import javax.persistence.Id;
+import javax.validation.constraints.NotEmpty;
+import javax.validation.constraints.NotNull;
+
+@Entity
+public class Lista implements Serializable{
+	
+	private static final long serialVersionUID = 1L;
+	
+	@Id
+	@GeneratedValue(strategy = GenerationType.AUTO)
+	private long id;
+	
+	@NotNull
+	@NotEmpty
+	private String nome;
+	
+	private List<String> itens;
+
+	public String getNome() {
+		return nome;
+	}
+
+	public void setNome(String nome) {
+		this.nome = nome;
+	}
+
+	public List<String> getItens() {
+		return itens;
+	}
+
+	public void setItens(List<String> itens) {
+		this.itens = itens;
+	}
+
+	public long getId() {
+		return id;
+	}
+}
