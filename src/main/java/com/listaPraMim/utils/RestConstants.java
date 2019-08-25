94dd5d047dcb5b5e97a8fdafcad03ba3be214dad
marsjunior <marcos.junio@ccc.ufcg.edu.br>
2019-08-24 21:12:50 -0300

    Define constantes rest

---
+++ src/main/java/com/listaPraMim/utils/RestConstants.java
@@ -0,0 +1,8 @@
+package com.listaPraMim.utils;
+
+public class RestConstants {
+	public static final String API_URI = "/api";
+	public static final String USUARIO_URI = API_URI + "/usuario";
+	public static final String LISTA_URI = API_URI + "/lista";
+	
+}
