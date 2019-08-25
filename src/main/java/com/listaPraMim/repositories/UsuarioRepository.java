839de02d39176e949d8fc471151f29aad298ead1
marsjunior <marcos.junio@ccc.ufcg.edu.br>
2019-08-24 21:07:31 -0300

    cria repositorio do usuario

---
+++ src/main/java/com/listaPraMim/repositories/UsuarioRepository.java
@@ -0,0 +1,11 @@
+package com.listaPraMim.repositories;
+
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
+
+import com.listaPraMim.models.Usuario;
+
+@Repository
+public interface UsuarioRepository extends JpaRepository<Usuario, Long>{
+
+}
